6112ec324f36772c1be6dda9ac203c84e614e98c
Nicole <79296688+nicolel2021@users.noreply.github.com>
2022-05-18 17:41:06 -0700

    server connect to db

--- server/index.js
+++ server/index.js
@@ -2,20 +2,25 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const pool = require('./db/db.js');
+const getReviews = require('./db/queries/getReviews');
 
 // middleware
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // GET
-app.get('/reviews', (req, res) => {})
-app.get('/reviews/meta', (req, res) => {})
+app.get('/', (request, response) => {
+  response.status(200).json({ info: 'Hello, conected!' })
+})
+app.get('/reviews', getReviews);
+// app.get('/reviews/meta', (req, res) => {})
 
 // POST
-app.post('/reviews', (req, res) => {})
+// app.post('/reviews', (req, res) => {})
 
 // PUT
-app.put('/reviews/:review_id/helpful', (req, res) => {})
+// app.put('/reviews/:review_id/helpful', (req, res) => {})
 
 
 app.listen(3000, ()=> {
