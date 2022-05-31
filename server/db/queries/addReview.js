b1159748a7ede2aecab02a4eba39cc02bdeab6c2
Nicole <79296688+nicolel2021@users.noreply.github.com>
2022-05-31 00:02:33 -0700

    Starting query, need to convert date

---
+++ server/db/queries/addReview.js
@@ -0,0 +1,18 @@
+const pool = require('../index');
+
+const addReview = (productId, rating, summary, body, recommend, name, email, date) => {
+  let queryString = `
+    INSERT INTO reviews (product_id, rating, date, summary, body, recommend, reported, reviewer_name, reviewer_email, response, helpfulness)
+    VALUES (
+      ${productId}, ${rating}, select extract(epoch from now()), ${summary}, ${body}, ${recommend}, false, ${name}, ${email}, null, 0
+    );`;
+
+  return pool
+    .query(queryString)
+    .then(()=> {
+      console.log('successfully added a review');
+    })
+    .catch(err => console.error('Error executing addReview query', err.stack))
+};
+
+module.exports = addReview;
