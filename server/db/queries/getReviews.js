b07e5a690621200867ea76732e31412f8b220eac
Nicole <79296688+nicolel2021@users.noreply.github.com>
2022-05-30 22:34:38 -0700

    Get getReviews and markReviewHelpful working

--- server/db/queries/getReviews.js
+++ server/db/queries/getReviews.js
@@ -6,20 +6,32 @@ const getReviews = (id, sort, count, page) => {
   } else if (sort === 'helpful') {
     sort = 'ORDER BY helpfulness DESC';
   } else if (sort === 'relevant') {
-    sort = 'ORDER BY date DESC, helpfulness DESC';
+    sort = 'ORDER BY helpfulness DESC, date DESC';
   }
 
   // console.log(id)
-  let queryString = `SELECT review_id, rating, summary, recommend, response, body, to_timestamp(date/1000) AS date, reviewer_name, helpfulness FROM reviews WHERE product_id = ${id} AND reported = false ${sort} LIMIT ${count};`;
-
+  // let queryString = `SELECT review_id, rating, summary, recommend, response, body, to_timestamp(date/1000) AS date, reviewer_name, helpfulness FROM reviews WHERE product_id = ${id} AND reported = false ${sort} LIMIT ${count};`;
+  // let photosQueryString = `SELECT photo_id as id, url FROM reviews_photos WHERE review_id = (SELECT review_id FROM reviews WHERE product_id = ${id} AND reported = false);`
+  let queryString = `
+    SELECT r.review_id, r.rating, r.summary, r.recommend, r.response, r.body, to_timestamp(r.date/1000) AS date, r.reviewer_name, r.helpfulness,
+    CASE WHEN count(o) = 0 THEN ARRAY[]::json[] ELSE array_agg(o.photo) END AS photos
+    FROM reviews r
+    LEFT OUTER JOIN
+    (
+    SELECT p.review_id, json_build_object('id', p.photo_id, 'url', p.url) as photo
+    FROM reviews_photos p
+	  ORDER BY p.review_id
+    ) o
+    ON r.review_id = o.review_id
+    WHERE r.product_id = ${id} AND r.reported = false
+    GROUP BY r.review_id
+    ${sort}
+    LIMIT ${count};
+    `;
   // promise
    return pool
     .query(queryString)
     .then(data => {
-      // console.log('data',data.rows)
-      for (var i = 0; i < data.rows.length; i++) {
-        data.rows[i].photos = [];
-      }
       return data.rows;
     })
     .catch(err => console.error('Error executing getReviews query', err.stack))
@@ -28,6 +40,6 @@ const getReviews = (id, sort, count, page) => {
 
 module.exports = getReviews;
 
-var s = new Date(1602494190229).toISOString()
+// var s = new Date(1602494190229).toISOString()
 // expected output "2020-10-12T09:16:30.229Z"
 // console.log(s);
--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ const cors = require('cors');
 const app = express();
 const pool = require('./db/index');
 const getReviews = require('./db/queries/getReviews');
+const markReviewHelpful = require('./db/queries/markReviewHelpful');
+const markReviewReported = require('./db/queries/markReviewReported');
 
 // middleware
 app.use(cors());
@@ -12,17 +14,14 @@ app.use(express.urlencoded({ extended: true }));
 // GET
 app.get('/', (request, response) => {
   response.status(200).json({ info: 'Hello, conected!' })
-})
+});
 
 app.get('/reviews', (req, res) => {
   let page = req.query.page || 1;
   let count = req.query.count || 5;
   let sort = req.query.sort || 'relevant';
   let id = req.query.product_id;
-  // console.log('page', page);
-  // console.log('count', count);
-  // console.log('sort', sort);
-  // console.log('id', id);
+
   getReviews(id, sort, count, page)
     .then(data => {
       // data.photo = [];
@@ -39,7 +38,7 @@ app.get('/reviews', (req, res) => {
       console.log('getReviews Error',err);
     })
 
-})
+});
 
 // app.get('/reviews/meta', (req, res) => {})
 
@@ -47,9 +46,38 @@ app.get('/reviews', (req, res) => {
 // app.post('/reviews', (req, res) => {})
 
 // PUT
-// app.put('/reviews/:review_id/helpful', (req, res) => {})
+app.put('/reviews/:review_id/helpful', (req, res) => {
+  let id = req.params.review_id;
+  console.log('review_id = ', id)
+
+  markReviewHelpful(id)
+    .then(() => {
+      console.log('Has been added to helpfulness');
+      res.status(204);
+      res.end();
+    })
+    .catch(err => {
+      console.log('markReviewHelpful Error',err);
+    })
+
+});
+
+app.put('/reviews/:review_id/report', (req, res) => {
+  let id = req.query.review_id;
+  console.log('review_id = ', id)
+
+  // markReviewReported(id)
+  //   .then(() => {
+  //     // console.log('The review has been reported.');
+  //     // res.status(204);
+  //   })
+  //   .catch(err => {
+  //     console.log('markReviewReported Error',err);
+  //   })
+
+});
 
 
 app.listen(3000, () => {
   console.log('server listening on port 3000')
-})
+});
