7d33675707e2c02eb26ce22d0ce1f54e72e47398
Nicole <79296688+nicolel2021@users.noreply.github.com>
2022-06-04 13:14:23 -0700

    wip

--- server/app.test.js
+++ server/app.test.js
@@ -1,9 +1,13 @@
 const request = require('supertest');
 const app = require('./app.js');
-const {pool, client} = require('../db/');
+const { pool, client } = require('../db/');
 
 const product_id = 94780;
 const review_id = 45972;
+
+afterAll(async () => {
+  await pool.end();
+});
 
 // describe('GET /reviews', () => {
 //   test('It should respond status code 200', async() => {
@@ -13,44 +17,69 @@ const review_id = 45972;
 // });
 
 describe('GET /reviews/meta', () => {
-  it('It should respond status code 200', async() => {
+  it('It should respond status code 200', async () => {
     const response = await request(app).get(`/reviews/meta?product_id=${product_id}`);
-    expect (response.statusCode).toBe(200);
+    expect(response.statusCode).toBe(200);
   });
 });
 
-describe('POST /reviews', () => {
-  it('It should response status code 201', async() => {
-    const formData = {
-      product_id: product_id,
-      rating: 5,
-      summary: "love it",
-      body: "I would recommend this product.",
-      recommend: true,
-      name: "john",
-      email: "john@gmail.com",
-      photos: ["dkljflkajoifjeoaihdoifhjalekjflakfjoiejhadfhoiej"],
-      characteristics: {
-          "5": 5,
-          "6": 5,
-          "7": 5
-      }
-  }
-    const response = await request(app).post('/reviews').send(formData);
-    expect (response.statusCode).toBe(201);
-  })
-});
+// describe('POST /reviews', () => {
+//   const formData = {
+//     product_id: product_id,
+//     rating: 5,
+//     summary: "love it",
+//     body: "I would recommend this product.",
+//     recommend: true,
+//     name: "john",
+//     email: "john@gmail.com",
+//     photos: ["dkljflkajoifjeoaihdoifhjalekjflakfjoiejhadfhoiej"],
+//     characteristics: {
+//       "5": 5,
+//       "6": 5,
+//       "7": 5
+//     }
+
+//   // afterEach(async () => {
+//   //   await pool.query(`DELETE FROM reviews WHERE answer_id = ${test_answer_id};`);
+//   //   await pool.query(`DELETE FROM reviews_photos WHERE answerer_name = '${testData.name}';`);
+//   // });
+
+//   it('It should response status code 201', async () => {
+//     const response = await request(app).post('/reviews').send(formData);
+//     expect(response.statusCode).toBe(201);
+//   })
+// });
 
 describe('PUT /reviews/:review_id/helpful', () => {
-  it('It should response status code 204', async() => {
-    const response = await request(app).put(`/reviews/:${review_id}/helpful`);
-    expect (response.statusCode).toBe(204);
+  let helpfulnessBeforeTest;
+  beforeEach(async () => {
+    const data = await pool.query(`SELECT helpfulness FROM reviews WHERE review_id = ${review_id}`);
+    helpfulnessBeforeTest = data.rows[0].helpfulness;
+  })
+
+  afterEach(async () => {
+    await pool.query(`UPDATE reviews SET helpfulness = ${helpfulnessBeforeTest} WHERE review_id = ${review_id};`);
+  });
+
+  it('It should response status code 204', async () => {
+    const response = await request(app).put(`/reviews/${review_id}/helpful`);
+    expect(response.statusCode).toBe(204);
   })
 });
 
 describe('PUT /reviews/:review_id/report', () => {
-  it('It should response status code 204', async() => {
-    const response = await request(app).put(`/reviews/:${review_id}/report`);
-    expect (response.statusCode).toBe(204);
+  let reportBeforeTest;
+  beforeEach(async () => {
+    const data = await pool.query(`SELECT reported FROM reviews WHERE review_id = ${review_id}`);
+    reportBeforeTest = data.rows[0].helpfulness;
+  })
+
+  afterEach(async () => {
+    await pool.query(`UPDATE reviews SET reported = false WHERE review_id = ${review_id};`);
+  });
+
+  it('It should response status code 204', async () => {
+    const response = await request(app).put(`/reviews/${review_id}/report`);
+    expect(response.statusCode).toBe(204);
   })
 });
