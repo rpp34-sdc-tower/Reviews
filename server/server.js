52446c5dd898faf47a636632b834a1454f4dff44
Nicole <79296688+nicolel2021@users.noreply.github.com>
2022-06-05 13:56:49 -0700

    Change port to 3005

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 const app = require('./app.js');
 
-app.listen(3000, () => {
-  console.log('server listening on port 3000')
+app.listen(3005, () => {
+  console.log('server listening on port 3005')
 });
